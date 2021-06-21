9430b2cee3877d0ee915ab315e67b2cb56b61554
unknown <10431012+moezakura@users.noreply.github.com>
2021-06-22 02:54:54 +0900

    画像をBase64にして返すAPIを追加

--- dll/cmd/dll/main.go
+++ dll/cmd/dll/main.go
@@ -2,10 +2,20 @@ package main
 
 import "C"
 import (
+	"bytes"
+	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
+	"os"
 
 	"github.com/moezakura/beatSong/dll/pkg/jsio"
 	"github.com/moezakura/beatSong/dll/pkg/song"
+	"golang.org/x/image/draw"
+	"golang.org/x/xerrors"
 )
 
 func main() {
@@ -25,8 +35,10 @@ func songList(root *C.char) *C.char {
 				Error:  err,
 			}
 		}
-		return &jsio.SongList{Status: true,
-			Payload: result}
+		return &jsio.SongList{
+			Status:  true,
+			Payload: result,
+		}
 	}()
 
 	j, err := json.Marshal(res)
@@ -35,4 +47,43 @@ func songList(root *C.char) *C.char {
 	}
 
 	return C.CString(string(j))
+}
+
+//export getImage
+func getImage(imagePath *C.char) *C.char {
+	ip := C.GoString(imagePath)
+
+	img, err := func() (string, error) {
+		f, err := os.Open(ip)
+		if err != nil {
+			return "", xerrors.Errorf("failed to open image: %w", err)
+		}
+		defer f.Close()
+
+		img, _, err := image.Decode(f)
+		if err != nil {
+			return "", xerrors.Errorf("failed to decode image: %w", err)
+		}
+
+		ss := img.Bounds()
+		ds := 80
+
+		imgDist := image.NewRGBA64(image.Rect(0, 0, ds, ds))
+		draw.CatmullRom.Scale(imgDist, imgDist.Bounds(), img, ss, draw.Over, nil)
+
+		var buf bytes.Buffer
+		if err := png.Encode(&buf, imgDist); err != nil {
+			return "", xerrors.Errorf("failed to encode image to png: %w", err)
+		}
+
+		b64 := base64.StdEncoding.EncodeToString(buf.Bytes())
+
+		return fmt.Sprintf("data:image/png;base64,%s", b64), nil
+	}()
+
+	if err != nil {
+		return C.CString(err.Error())
+	}
+
+	return C.CString(img)
 }
