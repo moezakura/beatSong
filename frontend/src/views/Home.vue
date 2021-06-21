7fa60bd0db11e07da605b459e2d28f0c4a427af3
unknown <10431012+moezakura@users.noreply.github.com>
2021-06-22 02:57:12 +0900

    楽曲リストをUIに表示いた

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class='home'>
+    <div v-for='s in songList' :key='s.dirPath'>
+      <img :src='s.imageBase64'>
+      <div>{{ s.name }}</div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script lang='ts'>
+import { computed, defineComponent, reactive, ref } from 'vue';
+import { SongInfo, ViewSongInfo } from '@/@types/global';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
   setup() {
-    //@ts-ignore
-    console.log(window.core.getSongList());
-  },
+    let songs = ref<SongInfo[]>([]);
+    const imageCache = reactive<Map<string, string>>(new Map<string, string>());
+
+    const songList = computed<ViewSongInfo[]>(() => {
+      return songs.value.filter(() => {
+        return true;
+      }).map(s => {
+        return {
+          ...s,
+          imageBase64: imageCache.get(s.imagePath) ?? ''
+        };
+      });
+    });
+
+    const getSong = async () => {
+      const res = await window.core.getSongList();
+      console.log(res);
+      songs.value = res.payload;
+
+      for (const s of songs.value) {
+        const image = await window.core.getImage(s.imagePath);
+        if (!image) {
+          console.log(image);
+        }
+        imageCache.set(s.imagePath, image);
+      }
+    };
+
+    getSong();
+
+    return {
+      songList,
+
+      getSong
+    };
+  }
 });
 </script>
